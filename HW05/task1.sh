902c53da0be951d07e60cbafb166b5359f98fc43
yushanlu <yushanlu1031@gmail.com>
2024-11-06 22:04:28 -0600

    update task1

--- HW05/task1.sh
+++ HW05/task1.sh
@@ -4,9 +4,8 @@
 #SBATCH --gpus-per-task=1 
 #SBATCH --output=FirstSlurm.out
 #SBATCH --error=FirstSlurm.err
-#SBATCH --cpus-per-task=4
 
 module load nvidia/cuda/11.8.0
-module load gcc/9.3.0
+module load gcc/9.4.0
 nvcc task1.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task1
 ./task1 >> FirstSlurm.out
