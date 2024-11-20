16a23f5f443fe8e302e75acbea9bdc730a205416
yushanlu <yushanlu1031@gmail.com>
2024-11-20 14:29:47 -0600

    Task 2 in progress

--- HW06/stencil.cu
+++ HW06/stencil.cu
@@ -77,9 +77,9 @@ __global__ void stencil_kernel(const float *image, const float *mask, float *out
         int shared_image_idx = R + local_idx + j;
 
         result += shared_image[shared_image_idx] * shared_mask[mask_idx];
-        if (global_idx == 5){
-            printf("j: %d, shared_image_idx: %d, shared_image[%d]: %f, mask: %f, result: %f\n", j, shared_image_idx, shared_image_idx, shared_image[shared_image_idx], shared_mask[mask_idx], result);
-        }
+        // if (global_idx == 5){
+        //     printf("j: %d, shared_image_idx: %d, shared_image[%d]: %f, mask: %f, result: %f\n", j, shared_image_idx, shared_image_idx, shared_image[shared_image_idx], shared_mask[mask_idx], result);
+        // }
     }
 
     // Write the result to global memory
