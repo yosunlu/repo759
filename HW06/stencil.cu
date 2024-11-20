e21709ef9cd7a09c04f4368825d65b87cd854cb6
yushanlu <yushanlu1031@gmail.com>
2024-11-20 03:49:34 -0600

    Task 2 in progress

--- HW06/stencil.cu
+++ HW06/stencil.cu
@@ -56,7 +56,7 @@ __global__ void stencil_kernel(const float *image, const float *mask, float *out
 
     // load right halo
     if (local_idx == blockDim.x - 1){
-        int shared_image_idx = block_idx + R;
+        int shared_image_idx = blockDim.x + R;
         for(int i = 1; i <= R; ++i){
             int right_idx = global_idx + i;
             shared_image[shared_image_idx] = right_idx >= n ? 1.0f : image[global_idx];
