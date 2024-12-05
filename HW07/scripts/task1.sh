01138ee272d79b58cae81aa6877b3a3a51ab11eb
yushanlu <yushanlu1031@gmail.com>
2024-12-05 01:09:13 -0600

    task1

--- HW07/scripts/task1.sh
+++ HW07/scripts/task1.sh
@@ -11,5 +11,5 @@
 module load nvidia/cuda/11.8.0
 module load gcc/9.4.0
 nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o task1
-./task1 1024 32
+./task1 1024 64
 rm task1
