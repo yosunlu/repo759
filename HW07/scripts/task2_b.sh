1b9051f9bfff9cb2797d92e50d943f58eec2a5fb
yushanlu <yushanlu1031@gmail.com>
2024-12-06 02:31:11 -0600

    task2

---
+++ HW07/scripts/task2_b.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=task2
+#SBATCH -p instruction
+#SBATCH --ntasks=1 --cpus-per-task=2
+#SBATCH --time=0-04:00:00
+#SBATCH --output="./task2.out"
+#SBATCH --error="./task2.err"
+#SBATCH --gres=gpu:1
+
+# Load necessary modules
+module load nvidia/cuda/11.8.0
+module load gcc/9.4.0
+
+# Compile the program
+nvcc task2.cu reduce.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task2
+
+# Run the program for n = 2^10 to 2^30 with threads_per_block=256
+for ((i=10; i<=30; i++)); do
+    n=$((2**i))
+    echo "Running task2 for n = $n with threads_per_block=256" >> task2.out
+    ./task2 $n 256 >> task2.out
+    echo "Finished task2 for n = $n with threads_per_block=256" >> task2.out
+done
+
+# Run the program for n = 2^10 to 2^30 with threads_per_block=1024
+for ((i=10; i<=30; i++)); do
+    n=$((2**i))
+    echo "Running task2 for n = $n with threads_per_block=1024" >> task2.out
+    ./task2 $n 1024 >> task2.out
+    echo "Finished task2 for n = $n with threads_per_block=1024" >> task2.out
+done
+
+# Clean up
+rm task2
